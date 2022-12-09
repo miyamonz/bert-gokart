68f047c75e46b1118794191a4a8ab13bfba099a3
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-09 22:19:47 +0000

    Adding tarfile member sanitization to extractall()

--- tasks/data/livedoor.py
+++ tasks/data/livedoor.py
@@ -13,7 +13,29 @@ class DownloadLivedoorCourpus(gokart.TaskOnKart):
         local_filename, headers = urllib.request.urlretrieve(self.url, dir + "ldcc-20140209.tar.gz")
         extract_dir = self.local_temporary_directory + "ldcc-20140209"
         with tarfile.open(local_filename, "r:gz") as tar:
-            tar.extractall(extract_dir)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, extract_dir)
         self.dump(extract_dir)
 
 
